3d582aca75079eed41db26968990a7fbd1964f00
mubaraq <mubbyre6xx@gmail.com>
2022-06-10 00:17:19 +0100

    initial commit

---
+++ src/data.js
@@ -0,0 +1,76 @@
+const people = [
+  {
+    id: 1,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+    name: "maria ferguson",
+    title: "office manager",
+    quote:
+      "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
+  },
+  {
+    id: 2,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    name: "john doe",
+    title: "regular guy",
+    quote:
+      "Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.",
+  },
+  {
+    id: 3,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg",
+    name: "peter smith",
+    title: "product designer",
+    quote:
+      "Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.",
+  },
+  {
+    id: 4,
+    image:
+      "https://images.unsplash.com/photo-1605949257507-142fa54cfb61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+    name: "susan andersen",
+    title: "the boss",
+    quote:
+      "Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ",
+  },
+  {
+    id: 5,
+    image:
+      "https://media.istockphoto.com/photos/handsome-bearded-businessman-picture-id1334629371?k=20&m=1334629371&s=612x612&w=0&h=SuEakZj-iqGJgVULXiAWzosC6A9yUH1o4lY7T5fxpe8=",
+    name: "John Rodgers",
+    title: "Intern",
+    quote:
+      "Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ",
+  },
+  {
+    id: 6,
+    image:
+      "https://media.istockphoto.com/photos/smiling-young-indian-woman-enjoying-video-call-conversation-picture-id1387743677?k=20&m=1387743677&s=612x612&w=0&h=jUqr6PX4A58dzPlayM6fbAf5xV4WcsXjSiAkvWpJEZU=",
+    name: "karren Smith",
+    title: "Data Analyst",
+    quote:
+      "Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ",
+  },
+  {
+    id: 7,
+    image:
+      "https://media.istockphoto.com/photos/head-shot-portrait-confident-attractive-young-woman-standing-at-home-picture-id1309798073?k=20&m=1309798073&s=612x612&w=0&h=Dn5BMCtSyIK4ChEqc6lTNintDvcnyR-HS4SFPs62x5g=",
+    name: "scott parker",
+    title: "intern",
+    quote:
+      "Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ",
+  },
+  {
+    id: 8,
+    image:
+      "https://media.istockphoto.com/photos/close-up-photo-portrait-of-a-young-man-in-the-office-office-worker-a-picture-id1398995568?k=20&m=1398995568&s=612x612&w=0&h=qtmcjDMCvVQqKUOlNvtowL8tNBppRIH-zXyho7XjhFQ=",
+    name: "susan andersen",
+    title: "ordinary guy",
+    quote:
+      "Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ",
+  },
+];
+
+export default people;
